92f90d9b6d2b7bc9872fe7a2461356e7415f028a
Delcio Cruz <delciocruzoficial@outlook.com>
2021-09-29 19:28:52 -0300

    att 1-
    - A bibilioteca #include <locale.h> foi removida.
    - foi adicionado um loop (do_while) para repetição, caso o default seja acionado.
    - edit estético de espaçamento do codigo.

--- intro.c
+++ intro.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(){
- 	setlocale(LC_ALL, "portuguese");
+
 	int opcao;
-		
+	
+	    do{
 	printf("\t\t\t\t\t\t\t\tBem vindo usuario\n");
 	printf("\t\t\tAqui voce podera achar os comandos e tera acesso a suportes que voce precisara para programar em C.\n");
 	printf("\t\t\t\t     O objetivo deste programa a te dar acesso rapido a ferramentas da linguagem C.\n\n\n\n\n\n");
 	system("pause>null");
 	
-	printf("\t\t\t\t\t\t\t\tEscolha uma opçao:\n\n");
+	printf("\t\t\t\t\t\t\t\tEscolha uma opcao:\n\n");
 	printf("\t\t\t\t\t\t\t\t(1) Bibliotecas\n");
 	printf("\t\t\t\t\t\t\t\t(2) Comandos system\n");
 	printf("\t\t\t\t\t\t\t\t(3) Logica de Programacao\n");
@@ -23,34 +22,39 @@ int main(){
 	system("cls");
 	
 	switch (opcao){
-    case 1:
+   
+	case 1:
     	printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
     	system("pause>null");
-    break;
+    	break;
     
 	case 2:
-		printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
-		system("pause>null");
-    break;
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
+	system("pause>null");
+    	break;
     
 	case 3:
-		printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
-		system("pause>null");
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
+	system("pause>null");
 	break;
 	
-    case 4:
+    	case 4:
     	printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
     	system("pause>null");
-    break;
+    	break;
     
 	case 5:
-		printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
-		system("pause>null");
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
+	system("pause>null");
 	break;
 	default:
-		printf("\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\tA VALIDAR");
-		system("pause>null");
-		}
+	printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t COMANDO INVALIDO\n");
+	printf("t\t\t\t\t\t\t\t\t\t POR FAVOR DIGITE UMA OPCAO VALIDA");
+	system("pause>null");
+	system("cls");
+	break;
+	   }
+	}while(opcao < 0 || opcao >= 6);	
 	return 0;
 
 }
